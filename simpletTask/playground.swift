634fc07de729cdb0af91db6a8b75620a2c0a81f1
jummman5 <mhjummanan@gmail.com>
2021-08-27 00:30:25 +0200

    git first init

---
+++ simpletTask/playground.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func factorial(a: Int) -> Int{
+    let n = a
+    if(n == 1){
+      return 1
+    }else{
+      return n*factorial(a: (n-1))
+    }
+}
+print("This is the number of factorial,,,,, ? ", factorial(a:20))
