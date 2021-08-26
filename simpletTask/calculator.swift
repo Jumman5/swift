5d14575241ca0328b5be6675b343bbcc90d5fa89
jummman5 <mhjummanan@gmail.com>
2021-08-27 01:29:37 +0200

    ancora no

---
+++ simpletTask/calculator.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let number1 = Int( readLine()!)!
+print("your given number:", number1)
+let number2=Int( readLine()!)!
+print("your given number:", number2)
+
+func add(int1: Int, int2: Int) -> Int{
+    return 
+}
