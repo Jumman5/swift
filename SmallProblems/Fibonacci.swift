9e5d7133704fab40b24f21f58762e420c01774da
jummman5 <mhjummanan@gmail.com>
2021-09-02 13:48:27 +0200

    Another Memoization way to solve a problem

--- SmallProblems/Fibonacci.swift
+++ SmallProblems/Fibonacci.swift
@@ -9,9 +9,25 @@ func fibonacciRecursive(Number: Int) -> Int {
         return Number //base case
     }
     else{
-        return fibonacciRecursive(Number: Number - 2) + fibonacciRecursive(Number: Number-1)
+        return fibonacciRecursive(Number: Number - 1) + fibonacciRecursive(Number: Number-2)
         //recursive case
     }
 }
 
-print(fibonacciRecursive(Number: 0))
+print(fibonacciRecursive(Number: 4))
+
+var fibMemo: [Int: Int] = [0: 0, 1: 1] // global variable as old base case
+
+func FibonacciMemoization(_ Number: Int) -> Int {
+    if let result = fibMemo[Number]{
+        return result
+
+    }
+    else{
+        fibMemo[Number] = FibonacciMemoization(Number - 1) + FibonacciMemoization(Number - 2)
+    }
+    return fibMemo[Number]!
+       
+}
+
+print(FibonacciMemoization(50))
